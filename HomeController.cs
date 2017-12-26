c5d6b286af7a0db9c5451a323b18c7d788c17e2e
shyamalaramesh <shyamala_gg@yahoo.co.uk>
2017-12-26 12:32:54 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo6.Models;
+namespace MVCDemo6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SampleDBContext1 db = new SampleDBContext1();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Employees.ToList());
+        }
+        [HttpPost]
+        public ActionResult Delete(IEnumerable<int> employeeIdsToDelete)
+        {
+            db.Employees.Where(x => employeeIdsToDelete.Contains(x.ID)).ToList().ForEach(y=>db.Employees.Remove(y));
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
